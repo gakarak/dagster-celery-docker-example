b2d202e2e5d64f2236f277ade477965abaa9ba04
Alexande Kalinovsky (Dell) <gakarak@gmail.com>
2021-09-25 19:56:38 +0300

    update ppl for @composite_solid

--- workflows/dagster_test_pipeline.py
+++ workflows/dagster_test_pipeline.py
@@ -3,7 +3,7 @@
 from datetime import time
 from genericpath import isfile
 from dagster import (
-    pipeline, solid, 
+    pipeline, solid, composite_solid, 
     Output, OutputDefinition,
     DynamicOutputDefinition, 
     InputDefinition, ModeDefinition, 
@@ -56,7 +56,7 @@ def generate_subtasks(context, data: L[dict]):
 @solid(
     tags={'dagster-celery/queue': 'cpu'}
 )
-def process_image(context, sample: dict, sleep_sec: float = 0.1):
+def process_image(context, sample: dict, sleep_sec: float = 0.1) -> dict:
     time.sleep(sleep_sec)
     path_img = sample['path']
     img = skio.imread(path_img)
@@ -66,6 +66,25 @@ def process_image(context, sample: dict, sleep_sec: float = 0.1):
         # 'info': sample, 
         'mean': float(ret)
     }
+
+
+@solid(
+    tags={'dagster-celery/queue': 'cpu'}
+)
+def process_scalar(context, sample: dict, sleep_sec: float = 0.1) -> dict:
+    time.sleep(sleep_sec)
+    mean_value = 10. * sample['mean']
+    return {
+        'mean': float(mean_value)
+    }
+
+
+@composite_solid(
+    input_defs=[InputDefinition("sample", dict)],
+    # tags={'dagster-celery/queue': 'cpu'}
+)
+def process_image_2stage(sample: dict) -> dict:
+    return process_scalar(process_image(sample))
 
 
 
@@ -92,6 +111,15 @@ def map_pipeline():
 
 
 @pipeline
+def map_pipeline2():
+    files_info = get_files_paths()
+    ret_map = generate_subtasks(files_info).map(process_image_2stage)
+    ret = reduce_results(ret_map.collect())
+    # ret = ret_map
+    return ret
+
+
+@pipeline
 def solid_pipeline():
     files_info = get_files_paths()
     return files_info
@@ -103,10 +131,15 @@ def solid_pipeline_2():
     return files_info
 
 
+@pipeline
+def solid_pipeline_3():
+    files_info = get_files_paths()
+    return files_info
+
 
 @repository
 def map_pipeline_repo():
-    return [map_pipeline, solid_pipeline, solid_pipeline_2]
+    return [map_pipeline, map_pipeline2, solid_pipeline, solid_pipeline_2, solid_pipeline_3]
 
 
 def main(
@@ -120,7 +153,8 @@ def main(
         }
     }
     print(f'run_config = {run_config}')
-    ret = execute_pipeline(map_pipeline, run_config=run_config)
+    # ret = execute_pipeline(map_pipeline, run_config=run_config)
+    ret = execute_pipeline(map_pipeline2, run_config=run_config)
     print(f'!!! @ret = {ret}')
 
 
